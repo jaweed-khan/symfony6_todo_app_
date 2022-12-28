10719d60a19a97fcfc57a93d723bac797d781366
Jaweed Khan <mr.jaweedkhan@gmail.com>
2022-12-28 19:48:32 +0530

    worked on the changes with methods for todo list create, list, update and delete

--- src/Controller/TodoListController.php
+++ src/Controller/TodoListController.php
@@ -4,37 +4,79 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
+use App\Entity\Task;
+use Doctrine\Persistence\ManagerRegistry;
 
 class TodoListController extends AbstractController
 {
     #[Route('/todo/list', name: 'app_todo_list')]
-    public function index()
+    public function index(ManagerRegistry $doctrine)
     {
         $data = [
             'header' => 'Todo List',
             'description' => 'This is symfony todo list app',
         ];
 
+        /**
+         * Order by desc 
+         */
+
+        $tasks = $doctrine->getRepository(Task::class)->findBy([], ['id' => 'DESC']);
+
+        $data['tasks'] = $tasks;
+
         return $this->render('index.html.twig', $data);
     }
-
+    
     #[Route('/todo/create', name: 'app_todo_create', methods: ['POST'])]
-    public function create()
+    public function create(ManagerRegistry $doctrine, Request $request)
     {
-        exit('to do: create a new task');
+        $title = $request->request->get('title'); 
+
+        if(!$title)
+            return $this->redirectToRoute('app_todo_list');
+        
+        $task = new Task();
+        $task->setTitle($title);
+
+        $doctrine->getManager()->persist($task);
+        $doctrine->getManager()->flush();
+
+        return $this->redirectToRoute('app_todo_list');
+
+        
     }
 
     #[Route('/todo/switch-status/{id}', name: 'app_todo_switch_status')]
-    public function switchStatus($id)
+    public function switchStatus(ManagerRegistry $doctrine, $id)
     {
-        exit('to do: switch status of a task'. $id);
+        $task = $doctrine->getRepository(Task::class)->find($id);
+
+        if(!$task)
+            return $this->redirectToRoute('app_todo_list');
+
+        $task->setStatus(!$task->isStatus());
+
+        $doctrine->getManager()->persist($task);
+        $doctrine->getManager()->flush();
+
+        return $this->redirectToRoute('app_todo_list');
     }
 
     #[Route('/todo/delete/{id}', name: 'app_todo_delete')]
-    public function delete($id)
+    public function delete(ManagerRegistry $doctrine, $id)
     {
-        exit('to do: delete a task'. $id);
+        $task = $doctrine->getRepository(Task::class)->find($id);
+
+        if(!$task)
+            return $this->redirectToRoute('app_todo_list');
+
+        $doctrine->getManager()->remove($task);
+        $doctrine->getManager()->flush();
+
+        return $this->redirectToRoute('app_todo_list');
     }
 
 }
---
+++ src/Entity/Task.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\TaskRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: TaskRepository::class)]
+class Task
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $title = null;
+
+    #[ORM\Column]
+    private ?bool $status = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getTitle(): ?string
+    {
+        return $this->title;
+    }
+
+    public function setTitle(string $title): self
+    {
+        $this->title = $title;
+
+        return $this;
+    }
+
+    public function isStatus(): ?bool
+    {
+        return $this->status;
+    }
+
+    public function setStatus(bool $status): self
+    {
+        $this->status = $status;
+
+        return $this;
+    }
+}
